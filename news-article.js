1df6dfaa4ec9dd89b17744a04a5b3bb13704c55a
wolvthemes <88788674+wolvthemes@users.noreply.github.com>
2021-09-18 17:40:47 +0200

    Add files via upload

---
+++ news-article.js
@@ -0,0 +1,27 @@
+class NewsArticle extends HTMLElement {
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: 'open' });
+  }
+  set article(article) {
+    this.root.innerHTML = `
+          <style>
+           a,
+           a:visited {
+            text-decoration: none;
+            color: inherit;
+          }
+          
+           img {
+            width: 100%;
+          }
+          </style>
+          <a href="${article.url}">
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage ? article.urlToImage : ''}">
+            <p>${article.description}</p>
+          </a>`;
+  }
+}
+
+customElements.define('news-article', NewsArticle);
---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '4645bcf241c542279d15316495dd6566';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
